feb8f7de81125602e315a65fa746cdb16ed41bb5
Michael Johansyah <johansyahmichael@gmail.com>
2025-04-04 07:36:23 +0700

    Another improvement on grid function

--- script.js
+++ script.js
@@ -1,17 +1,40 @@
 const DEFAULT_SIZE = 16;
 
-const sketchArea = document.getElementById('sketch-area');
+const grid = document.getElementById("grid");
+const clearBtn = document.getElementById("clearBtn");
+
+let mouseDown = false;
+document.body.onmousedown = () => (mouseDown = true);
+document.body.onmouseup = () => (mouseDown = false);
+
+clearBtn.onclick = () => reloadGrid();
+
+function reloadGrid() {
+    clearGrid();
+    setupGrid(16);
+}
+
+function clearGrid() {
+    grid.innerHTML = "";
+}
 
 function setupGrid(size) {
-    sketchArea.style.gridTemplateColumns = `repeat(${size}, 1fr)`;
-    sketchArea.style.gridTemplateRows = `repeat(${size}, 1fr)`;
+    grid.style.gridTemplateColumns = `repeat(${size}, 1fr)`;
+    grid.style.gridTemplateRows = `repeat(${size}, 1fr)`;
 
     for (let i = 0; i < size * size; i++) {
         const gridElement = document.createElement("div");
         gridElement.classList.add("grid-element");
-        gridElement.style.border = "1px solid black"
-        sketchArea.appendChild(gridElement);
+        gridElement.style.border = "1px solid black";
+        gridElement.addEventListener("mouseover", changeColor);
+        gridElement.addEventListener("mousedown", changeColor);
+        grid.appendChild(gridElement);
     }
+}
+
+function changeColor(e) {
+    if (e.type === "mouseover" && !mouseDown) return;
+    e.target.style.backgroundColor = "black";
 }
 
 window.onload = () => {
